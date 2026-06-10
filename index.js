56eff612ab2b17f8eb85fa0c8f07e3b6968c51dd
ephellon <minkcbos@gmail.com>
2026-06-09 23:32:27 -0600

    Better layout

--- index.js
+++ index.js
@@ -1,8 +1,75 @@
-/** Peer Assessment
- * 1) Rank each category by importance
- * 2) Rank each person by relevance to each category
- * 3) Display results
+/***
+ *      ______       _              _   _
+ *     |  ____|     | |            | | | |
+ *     | |__   _ __ | |_ ___ _ __  | |_| |__   ___
+ *     |  __| | '_ \| __/ _ \ '__| | __| '_ \ / _ \
+ *     | |____| | | | ||  __/ |    | |_| | | |  __/
+ *     |______|_| |_|\__\___|_|     \__|_| |_|\___|        _
+ *         | |           | |          | |                 | |
+ *      ___| |_ _   _  __| | ___ _ __ | |_   _ __ ___  ___| |_ ___ _ __
+ *     / __| __| | | |/ _` |/ _ \ '_ \| __| | '__/ _ \/ __| __/ _ \ '__|
+ *     \__ \ |_| |_| | (_| |  __/ | | | |_  | | | (_) \__ \ ||  __/ |
+ *     |___/\__|\__,_|\__,_|\___|_| |_|\__| |_|  \___/|___/\__\___|_|
+ *      _          _                             _   _            _
+ *     | |        | |                           | | | |          | |
+ *     | |__   ___| |___      _____  ___ _ __   | |_| |__   ___  | |___      _____
+ *     | '_ \ / _ \ __\ \ /\ / / _ \/ _ \ '_ \  | __| '_ \ / _ \ | __\ \ /\ / / _ \
+ *     | |_) |  __/ |_ \ V  V /  __/  __/ | | | | |_| | | |  __/ | |_ \ V  V / (_) |
+ *     |_.__/ \___|\__| \_/\_/ \___|\___|_| |_|  \__|_| |_|\___|  \__| \_/\_/ \___/
+ *                                                          | |
+ *       __ _ _ __ __ ___   _____    __ _  ___ ___ ___ _ __ | |_ ___
+ *      / _` | '__/ _` \ \ / / _ \  / _` |/ __/ __/ _ \ '_ \| __/ __|
+ *     | (_| | | | (_| |\ V /  __/ | (_| | (_| (_|  __/ | | | |_\__ \_
+ *      \__, |_|  \__,_| \_/ \___|  \__,_|\___\___\___|_| |_|\__|___(_)
+ *       __/ |
+ *      |___/
  */
+`
+
+Alexander
+Burkholder
+Cain
+Chase
+Fountain
+Grey
+Julius
+LaPlante
+Matt
+Nail
+Richards
+Rogers
+Saez
+Scott
+Sweeney
+Yeager
+
+`
+/***
+ *       _____ _______ ____  _____
+ *      / ____|__   __/ __ \|  __ \
+ *     | (___    | | | |  | | |__) |
+ *      \___ \   | | | |  | |  ___/
+ *      ____) |  | | | |__| | |
+ *     |_____/   |_|  \____/|_|
+ *
+ *
+ */
+/***
+ *      _____              _ _     _                   _
+ *     |  __ \            ( ) |   | |                 | |
+ *     | |  | | ___  _ __ |/| |_  | |_ ___  _   _  ___| |__
+ *     | |  | |/ _ \| '_ \  | __| | __/ _ \| | | |/ __| '_ \
+ *     | |__| | (_) | | | | | |_  | || (_) | |_| | (__| | | |
+ *     |_____/ \___/|_| |_|  \__|  \__\___/ \__,_|\___|_| |_|  _
+ *                       | | | |   (_)             | |        | |
+ *       __ _ _ __  _   _| |_| |__  _ _ __   __ _  | |__   ___| | _____      __
+ *      / _` | '_ \| | | | __| '_ \| | '_ \ / _` | | '_ \ / _ \ |/ _ \ \ /\ / /
+ *     | (_| | | | | |_| | |_| | | | | | | | (_| | | |_) |  __/ | (_) \ V  V /
+ *      \__,_|_| |_|\__, |\__|_| |_|_|_| |_|\__, | |_.__/ \___|_|\___/ \_/\_(_)
+ *                   __/ |                   __/ |
+ *                  |___/                   |___/
+ */
+.trim().split(/[\n\r\f]+/).filter(n=>n).map(n=>(top.peers||=[]).push(n));
 /***
  *       _____
  *      / ____|
@@ -446,6 +513,11 @@ function toTimeString(milliseconds = 0, format = 'natural') {
  *                    __/ |
  *                   |___/
  */
+/** Peer Assessment - How it works
+ * 1) Rank each category by importance
+ * 2) Rank each person by relevance to each category
+ * 3) Display results
+ */
 location.hash = -1;
 
 let opts = {
@@ -460,25 +532,6 @@ let opts = {
 };
 
 let cats = new Sortable($('#cats'), opts);
-
-let peers = [
-    'Alexander',
-    'Burkholder',
-    'Cain',
-    'Chase',
-    'Fountain',
-    'Grey',
-    'Julius',
-    'LaPlante',
-    'Matt',
-    'Nail',
-    'Richards',
-    'Rogers',
-    'Saez',
-    'Scott',
-    'Sweeney',
-    'Yeager',
-];
 
 $('#name').append(
     ...peers.map((name, value) => furnish('option').with(name))
@@ -585,6 +638,9 @@ $('#prev').addEventListener('mouseup', event => {
     $('footer').dataset.step = ind + 2;
 });
 
-setInterval(due => {
-    due.innerText = toTimeString(new Date('Dec 12 2022 08:00:00 GMT-0700') - new Date);
-}, 100, $('#due'));
+let due23_2 = new Date('Dec 12 2022 08:00:00 GMT-0700');
+
+if(new Date < due23_2)
+    setInterval(due => {
+        due.innerHTML = `&mdash; Due in ${ toTimeString(due23_2 - new Date) }.`;
+    }, 100, $('#due'));
